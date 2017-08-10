10ec7cd9079817d1fdc4533b9ee3ed08ad186c67
liqiang <qiang.li@ployun.com>
2017-08-10 17:47:38 +0800

    李强平台提交:后台_知识相关

--- lq-plat-link-web/src/main/java/com/lq/plat/link/controller/portal/KnowledgeController.java
+++ lq-plat-link-web/src/main/java/com/lq/plat/link/controller/portal/KnowledgeController.java
@@ -6,6 +6,7 @@ import com.lq.plat.link.knowledage.KnowledgeQuestionUpdatePara;
 import com.lq.plat.link.service.KnowledgeAnswerService;
 import com.lq.plat.link.service.KnowledgeQuestionService;
 import com.lq.plat.link.utils.ApiUtils;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.WebAsyncTask;
 
 import javax.validation.Valid;
@@ -26,6 +28,8 @@ import java.util.concurrent.Callable;
  * @version 1.0.0
  * @date 2017/8/9
  */
+@RestController
+@Api(value = "后台_知识相关服务", description = "后台_知识相关接口")
 public class KnowledgeController  extends PlatformController {
 
     @Autowired
@@ -37,7 +41,7 @@ public class KnowledgeController  extends PlatformController {
 
 
     //当确认收到钱之后，后台将该问题状态设为已发布
-    @ApiOperation(value = "更新答案信息", notes = "添加返回的结果")
+    @ApiOperation(value = "更新答案信息:当确认收到钱之后，后台将该问题状态设为已发布", notes = "添加返回的结果")
     @PutMapping("manage/knowledgequestions")
     public WebAsyncTask<ResponseEntity<PlatformResult>> updateInfoAccounts(
             @Valid @RequestBody final KnowledgeQuestionUpdatePara knowledgeQuestionUpdatePara, final BindingResult result) {
